e9f6b24517b63a546aab190cf4792dc4e208d8a7
Logan Rickert <logan@rickertcorp.com>
2017-03-30 03:59:04 +0000

    Added about page

--- index.php
+++ index.php
@@ -1,13 +1,73 @@
 <?PHP
     echo file_get_contents('src/header.html');
 ?>
-            <div class="wrapper">
+            <div class="home-bg">
                 <div class="wrapper">
-                <h1>Cloud LC3</h1>
-                <section class="content">
-                    <p>Welcome to the Cloud LC3 website!</p>
-                </section>
+                    <h1>CloudLC3</h1>
+                    <section class="content">
+                        <p>A new way to code LC3 - FREE!</p>
+                        <p><a href="machine.php">Get Started!</a></p>
+                        <img src="images/lc31.png">
+                    </section>
+                </div>
             </div>
+            <div class="wrapper">
+                <div class="content-a">
+                    <p class="left">Editor with line numbers</p>
+                    <img class="right" src="images/lc32.png">
+                </div>
+            </div>
+            <div class="home-blue">
+                <div class="wrapper">
+                    <div class="content-a">
+                        <img class="right" src="images/lc33.png">
+                        <p class="left">Live, detailed error checking</p>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="content-a">
+                    <p class="left">Real-time converter calculator</p>
+                    <img class="right" src="images/lc34.png">
+                </div>
+            </div>
+            <div class="home-blue">
+                <div class="wrapper">
+                    <div class="content-a">
+                        <img class="right" src="images/lc35.png">
+                        <p class="left">Quick reference for instructions</p>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="content-a">
+                    <p class="left">Work in progress Graphical User Interface</p>
+                    <img class="right" src="images/lc36.png">
+                </div>
+            </div>
+            <div class="home-blue">
+                <div class="wrapper">
+                    <div class="content-a">
+                        <img class="right" src="images/lc39.png">
+                        <p class="left">Edit Instructions and Memory in-line by just hovering over the value</p>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="content-a">
+                    <p class="left">Set and unset breakpoints - "b_" labels autoset</p>
+                    <img class="right" src="images/lc38.png">
+                </div>
+            </div>
+            <div class="home-blue">
+                <div class="wrapper">
+                    <div class="content-a">
+                        <img class="right" src="images/lc37.png">
+                        <p class="left">Easy to use console</p>
+                    </div>
+                </div>
+            </div>
+            
 <?PHP
     echo file_get_contents('src/footer.html');
 ?>
